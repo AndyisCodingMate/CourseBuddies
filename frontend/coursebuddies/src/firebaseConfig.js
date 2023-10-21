0057495ffa78a169167b6a6418034e4221d5ff79
Andy Poon <136886033+AndyisCodingMate@users.noreply.github.com>
2023-10-21 09:42:27 -0700

    Firebase setup complete
    Complete hosting and firebase setup. Domain are sent in Discord and json files are also customized for domain too.

--- frontend/coursebuddies/src/firebaseConfig.js
+++ frontend/coursebuddies/src/firebaseConfig.js
@@ -1,12 +1,19 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {  
-
+    apiKey: "AIzaSyBOEodUKiBpVXXl7STHNqx3Y8_rNtUmDcQ",
+    authDomain: "coursebuddies-28099.firebaseapp.com",
+    projectId: "coursebuddies-28099",
+    storageBucket: "coursebuddies-28099.appspot.com",
+    messagingSenderId: "1005785832412",
+    appId: "1:1005785832412:web:b90b6d414220bf98520d19",
+    measurementId: "G-0M9KD6YRDS"
 };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-if (firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-export default firebase;
+export {firebase, app, analytics};
