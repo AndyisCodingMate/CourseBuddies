17431ea04dbfc52b437ebe0599c7ffe3f9a1eb90
Andy Poon <136886033+AndyisCodingMate@users.noreply.github.com>
2023-10-21 08:41:16 -0700

    Added Firebase UI
    Added FIrebase UI and config files. Will need to add more to complete drop-in.

---
+++ frontend/coursebuddies/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {  
+
+};
+
+if (firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
