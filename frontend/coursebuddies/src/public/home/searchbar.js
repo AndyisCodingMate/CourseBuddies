688728f389686f2d20a8614a4f51dafee6173dff
juppal15 <jatinuppal5@yahoo.com>
2023-10-21 11:58:49 -0700

    This is home page, rec club, rec friend, and search bar implementation
    Implementing the search bar and home page and rec club and rec friend functions

---
+++ frontend/coursebuddies/src/public/home/searchbar.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  const classes = ['Math', 'Science', 'History'];
+  const clubs = ['Chess', 'Music', 'Dance'];
+  const users = ['John', 'Jane', 'Doe'];
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const performSearch = (array) => {
+    return array.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  const filteredClasses = performSearch(classes);
+  const filteredClubs = performSearch(clubs);
+  const filteredUsers = performSearch(users);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <select onChange={handleFilterChange} value={filter}>
+        <option value="all">All</option>
+        <option value="classes">Classes</option>
+        <option value="clubs">Clubs</option>
+        <option value="users">Users</option>
+      </select>
+      <div className="results">
+        {filter === 'all' || filter === 'classes' ? (
+          <div>
+            <h2>Classes</h2>
+            {filteredClasses.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        ) : null}
+        {filter === 'all' || filter === 'clubs' ? (
+          <div>
+            <h2>Clubs</h2>
+            {filteredClubs.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        ) : null}
+        {filter === 'all' || filter === 'users' ? (
+          <div>
+            <h2>Users</h2>
+            {filteredUsers.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default App;
