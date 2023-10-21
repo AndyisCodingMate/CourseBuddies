0a201a2ec57f13de3fce3d3031cd1053a97392e5
juppal15 <jatinuppal5@yahoo.com>
2023-10-21 14:36:30 -0700

    this is firebase ui sign in
    login

--- frontend/coursebuddies/src/FirebaseUI.jsx
+++ frontend/coursebuddies/src/FirebaseUI.jsx
@@ -5,25 +5,43 @@ import * as firebaseui from 'firebaseui';
 
 const FirebaseUI = () => {
   useEffect(() => {
-    // Initialize the FirebaseUI widget using Firebase
+    // Initialize the FirebaseUI widget using Firebase.
     const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-    // FirebaseUI config options
     const uiConfig = {
-      signInSuccessUrl: '/',
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          // User successfully signed in.
+          // You can redirect here.
+          return true;
+        },
+        uiShown: function () {
+          // Hide loader.
+          document.getElementById('loader').style.display = 'none';
+        },
+      },
+      signInFlow: 'popup',
+      signInSuccessUrl: '/',  // url to redirect to on success
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // add other providers you want to support
       ],
+      tosUrl: '/terms-of-service',  // your terms of service url
+      privacyPolicyUrl: '/privacy-policy',  // your privacy policy url
     };
 
-    // Check if there's a pending email link sign-in or other redirect operation
-    if (ui.isPendingRedirect() || firebase.auth().isSignInWithEmailLink(window.location.href)) {
-      ui.start('#firebaseui-auth-container', uiConfig);
-    }
+    // Start the UI.
+    ui.start('#firebaseui-auth-container', uiConfig);
   }, []);
 
-  return <div id="firebaseui-auth-container"></div>;
+  return (
+    <div>
+      <h1>Welcome to My Awesome App</h1>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
+    </div>
+  );
 };
 
 export default FirebaseUI;
